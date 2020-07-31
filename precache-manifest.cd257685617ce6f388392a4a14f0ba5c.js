78e8a778cc18cafe64d07bbd35d19cd2b5fc11be
sandeshdeshmukh1 <stdeshmukh@mitaoe.ac.in>
2020-07-31 06:36:45 +0530

    Updates

---
+++ precache-manifest.cd257685617ce6f388392a4a14f0ba5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3dff30c15de481767c089e5bb4b7863",
+    "url": "/Video-conferencing-app/index.html"
+  },
+  {
+    "revision": "e876723454cffc702a09",
+    "url": "/Video-conferencing-app/static/css/main.80bf7588.chunk.css"
+  },
+  {
+    "revision": "3339f04b89567858a4b0",
+    "url": "/Video-conferencing-app/static/js/2.9a244244.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Video-conferencing-app/static/js/2.9a244244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e876723454cffc702a09",
+    "url": "/Video-conferencing-app/static/js/main.a12d8fff.chunk.js"
+  },
+  {
+    "revision": "bbeefb068b81afd275d4",
+    "url": "/Video-conferencing-app/static/js/runtime-main.27844fbb.js"
+  }
+]);
